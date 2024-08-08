f674a488e7d38c9b23a77f917b80cb5b25e099f0
Saliinger <88099624+Saliinger@users.noreply.github.com>
2024-08-09 01:53:34 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,141 +1,141 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   gnl.c                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: anoukan <anoukan@student.1337.ma>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/02 16:37:07 by anoukan           #+#    #+#             */
-/*   Updated: 2024/08/02 16:54:42 by anoukan          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
 int ft_strlen(char *str)
 {
-    int i = 0;
-    while (str[i])
-        i++;
-    return i;
+	int i = 0;
+	while (str[i])
+		i++;
+	return i;
 }
 
-bool is_line(char *s)
+bool ft_strchr(char *s)
 {
-    int i = 0;
-    while (s[i])
-    {
-        if (s[i] == '\n')
-            return true;
-        i++;
-    }
-    return false;
+	if (!s)
+		return NULL;
+	int i = 0;
+	while (s && s[i])
+	{
+		if (s[i] == '\n')
+			return true;
+		i++;
+	}
+	return false;
 }
 
 char *ft_strjoin(char *s1, char *s2)
 {
-    char *dest;
-    int i = 0;
-    int j = 0;
+	char *res;
+	int i = 0;
+	int j = 0;
 
-    dest = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char ));
-    if (!dest)
-        return (free(s1), NULL);
-    while (s1[i])
-    {
-        dest[i] = s1[i];
-        i++;
-    }
-    while (s2[j])
-    {
-        dest[i] = s2[j];
-        i++;
-        j++;
-    }
-    dest[i] = '\0';
-    return dest;
+	if (!s1 && !s2)
+		return NULL;
+	if (!(res = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1)))
+		return free(s1), NULL;
+	while (s1 && s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		res[i] = s2[j];
+		i++;
+		j++;
+	}
+	if (s1)
+		free(s1);
+	res[i] = '\0';
+	return res;
 }
 
-char *extract(char *s)
+char *extract_line(char *s)
 {
-    int i = 0;
-    int j = 0;
-    char *line;
+	char *line;
+	int i = 0;
 
-    while (s[i] && s[i] != '\n')
-        i++;
-    i++;
-    line = (char *)malloc(sizeof(char) * i + 2);
-    if (!line)
-        return free(s), NULL;
-    while (s[j] && s[j] != '\n')
-    {
-        line[j] = s[j];
-        j++;
-    }
-    if (s[j] == '\n')
-    {
-        line[j] = '\n';
-        j++;
-    }
-    line[j] = '\0';
-    return line;
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (s[i] == '\n')
+		i++;
+	if (!(line = (char *)malloc(sizeof(char) * i + 2)))
+		return free(s), NULL;
+	i = 0;
+	while (s[i] && s[i] != '\n')
+	{
+		line[i] = s[i];
+		i++;
+	}
+	if (s[i] == '\n')
+	{
+		line[i] = '\n';
+		i++;
+	}
+	line[i] = '\0';
+	return line;
 }
 
 char *clean(char *s)
 {
-    char *clean;
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
+	char *clean;
 
-    while (s[i] && s[i] != '\n')
-        i++;
-    i++;
-    clean = (char *)malloc((ft_strlen(s) - i + 1) * sizeof(char ));
-    if (!clean)
-        return free(s), NULL;
-    while (s[i])
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (!s[i]) // No newline found, free and return NULL
     {
-        clean[j] = s[i];
-        j++;
-        i++;
+		if (s)
+			free(s);
+        return NULL;
     }
-    clean[j] = '\0';
-    return clean;
+	if (!(clean = (char *)malloc(sizeof(char) * (ft_strlen(s) - i + 1))))
+		return free(s), NULL;
+	if (s[i] == '\n')
+		i++;
+	while (s[i])
+	{
+		clean[j] = s[i];
+		i++;
+		j++;
+	}
+	clean[j] = '\0';
+	if (s)
+		free(s);
+	return clean;
 }
 
 char *get_next_line(int fd)
 {
-    char *line;
-    char buffer[BS + 1];
-    static char *stash;
-    int bytes = 0;
-
-    if (!stash)
-    {
-        stash = (char *)malloc(sizeof (char));
-        if (!stash)
-            return NULL;
-        stash[0] = '\0';
-    }
-    bytes = read(fd, buffer, BS);
-    if (bytes == -1)
-        return free(stash), NULL;
-    if (bytes < BS && ft_strlen(stash) == 0)
-        return free(stash), NULL;
-    buffer[bytes] = '\0';
-    stash = ft_strjoin(stash, buffer);
-    while (is_line(stash) == false)
-    {
-        bytes = read(fd, buffer, BS);
-        if (bytes == -1)
-            return free(stash), NULL;
-        buffer[bytes] = '\0';
-        stash = ft_strjoin(stash, buffer);
-        if (bytes < BS)
-            break ;
-    }
-    line = extract(stash);
-    stash = clean(stash);
-    return line;
+	char *line;
+	char buffer[BUFFER_SIZE + 1];
+	static char *stash;
+	int bytes;
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return NULL;
+	if(!stash)
+	{
+		if (!(stash = (char *)malloc(sizeof(char))))
+			return NULL;
+		stash[0] = '\0';
+	}
+	while (ft_strchr(stash) == false && (bytes = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[bytes] = '\0';
+		stash = ft_strjoin(stash, buffer);
+	}
+	if (!stash)
+		return NULL ;
+	line = extract_line(stash);
+	stash = clean(stash);
+	if (line && line[0] == '\0')
+	{
+		if (line)
+			free(line);
+		if (stash)
+			free(stash);
+		return NULL;
+	}
+	return line;
 }
