36f27564f0e03a9b7f9b7e178e1aeb8d425d1bbe
Saliinger <88099624+Saliinger@users.noreply.github.com>
2024-08-08 19:25:00 +0200

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,86 +1,84 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anoukan <anoukan@student.42mulhouse.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 17:06:50 by anoukan           #+#    #+#             */
+/*   Updated: 2024/08/08 17:06:52 by anoukan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <limits.h>
 #include <unistd.h>
 #include <stdarg.h>
-#include <stdio.h>
 
-void ft_putchar(char c)
+int	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	return	write(1, &c, 1);
 }
 
-void ft_putstr(char *s, int *count)
+void	ft_putstr(char *s, int *count)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        ft_putchar(s[i]);
-        i++;
-    }
-    *count += i - 1;
-}
-
-void print_num(long long int num, int base, int *count)
-{
-	char *hexa = "0123456789abcdef";
-
-
-	if (num < 0)
+	if(!s)
 	{
-		num *= -1;
-		*count += 1; 
-        ft_putchar('-');
+		ft_putstr("(null)", count);
+		return;
 	}
+	while (s[i])
+	{
+		*count += ft_putchar(s[i]);
+		i++;
+	}
+}
+
+void	print_num(long long num, int base, int *count)
+{
+	char *hex = "0123456789abcdef";
+
+	if (num == INT_MIN)
+	{
+		ft_putstr("-2147483648", count);
+		return ;
+	}
+	if (num < 0)
+		*count += ft_putchar('-');
 	if (num >= base)
-		print_num((num / base), base, count);
-    *count +=1;
-    ft_putchar(hexa[num % base]);
-	return ;
+		print_num(num / base, base, count);
+	*count += ft_putchar(hex[num % base]);
 }
 
-void flag(int *count, char c, va_list *ap)
+void	selector(va_list *ap, int *count, char flag)
 {
-    if (c == 's')
-        ft_putstr(va_arg(*ap, char *), count);
-    else if (c == 'd')
-    {
-      int arg = va_arg(*ap, int);
-      printf("arg: %d\n", arg);
-      print_num(arg, 10, count);
-    }
-    else if (c == 'x')
-        print_num(va_arg(*ap, unsigned int ), 16, count);
+	if (flag == 's')
+		ft_putstr(va_arg(*ap, char *), count);
+	else if (flag == 'd')
+		print_num(va_arg(*ap, int), 10, count);
+	else if (flag == 'x')
+		print_num(va_arg(*ap, unsigned int), 16, count);
 }
 
-
-int ft_printf(const char *format, ... )
+int	ft_printf(char *format, ...)
 {
-    int count = 0;
-    int i = 0;
-    va_list ap;
+	int count = 0;
+	int i = 0;
+	va_list ap;
 
-    va_start(ap, format);
-    while (format[i])
-    {
-        if (format[i] == '%' && (format[i + 1] == 's' || format[i + 1] == 'd' || format[i + 1] == 'x'))
-        {
-            flag(&count, format[i + 1], &ap);
-            i++;
-        }
-        else
-        {
-            count++;
-            ft_putchar(format[i]);
-        }
-        i++;
-    }
-    va_end(ap);
-    return count;
-}
-
-int main(void)
-{
-    ft_printf("%s\n%d\n%x\n", "hoolall adlkfl asdf", -2147483648, 15);
-    printf("%s\n%ld\n%x\n", "hoolall adlkfl asdf", -2147483648, 15);
-    return 0;
+	va_start(ap, format);
+	while(format[i])
+	{
+		if (format[i] == '%' && (format[i + 1] == 's' || format[i + 1] == 'd' ||format[i + 1] == 'x'))
+		{
+			selector(&ap, &count, format[i + 1]);
+			i++;
+		}
+		else 
+			count += ft_putchar(format[i]);
+		i++;
+	}
+	va_end(ap);
+	return count;
 }
